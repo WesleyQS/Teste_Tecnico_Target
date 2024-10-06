48c399e1630502395dbf03fe32cdc5692c713d24
Wesley Queiroz <82420058+WesleyQS@users.noreply.github.com>
2024-10-06 01:52:48 -0300

    Update Teste_1.js

--- Teste Target/Teste_1.js
+++ Teste Target/Teste_1.js
@@ -1,3 +1,12 @@
+/*
+
+1) Observe o trecho de código abaixo: int INDICE = 13, SOMA = 0, K = 0;
+Enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; }
+Imprimir(SOMA);
+Ao final do processamento, qual será o valor da variável SOMA?
+
+*/
+
 let indice = 13;
 let soma = 0;
 let k = 0;
